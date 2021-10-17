5dcc7ade941513a8c42686ad23c5dfd31f12d05e
Jacob Speakman <speakman610@gmail.com>
2021-10-16 21:53:44 -0600

    added automatic hello message from bot

--- javascript.js
+++ javascript.js
@@ -18,7 +18,18 @@ document.getElementById('chatSubmit').addEventListener('click', function (event)
     });
 });
 
-// document.getElementById('botInput').addEventListener('keypress', function () {
-//     var randomColor = Math.floor(Math.random()*16777215).toString(16);
-//     document.getElementById('botInput').style.color = '#' + randomColor;
-// });
+function hello() {
+    const message = 'Hello';
+    const API_KEY = 'JWm9LD7K8RA3';
+    fetch(`https://api.pgamerx.com/v5/ai?message=${message}&server=main`, {
+        method: "get",
+        headers: {
+            "Authorization": API_KEY
+        }
+    }).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        botMessage = '<p class="botMessage">' + json[0].response + '</p>';
+        document.getElementById('chatbox').innerHTML = botMessage;
+    });
+}
