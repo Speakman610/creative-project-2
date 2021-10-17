4a1c51ea00ec55698c7a66ece67a1a6d0bac11cb
Jacob Speakman <speakman610@gmail.com>
2021-10-16 18:29:05 -0600

    added grayed out styling

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ document.getElementById('botSubmit').addEventListener('click', function (event) 
      called fetch which you can use*/
     event.preventDefault();
     const message = document.getElementById('botInput').value;
-    const API_KEY = 'JWm9LD7K8RA3'
+    const API_KEY = 'JWm9LD7K8RA3';
     // const API_KEY = process.env.API_KEY
     // get one api key at pgamerx.com/api/register
     fetch(`https://api.pgamerx.com/v5/ai?message=${message}&server=main`, {
@@ -18,4 +18,8 @@ document.getElementById('botSubmit').addEventListener('click', function (event) 
     }).then(function (json) {
         document.getElementById('botOutput').innerHTML = json[0].response;
     });
+});
+
+document.getElementById('botInput').addEventListener('keypress', function () {
+    document.getElementsByClassName('pageObject').style = "background-color = red";
 });
