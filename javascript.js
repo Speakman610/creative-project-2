53a91ab7ffb291eb012929431c3a32896deae4f4
Jacob Speakman <speakman610@gmail.com>
2021-10-16 21:34:27 -0600

    created sleek chat interface

--- javascript.js
+++ javascript.js
@@ -1,14 +1,11 @@
-document.getElementById('botSubmit').addEventListener('click', function (event) {
-    // const fetch = require('node-fetch')
-    /* if you are using vanilla javascript,
-    there is an in-built function
-     called fetch which you can use*/
+document.getElementById('chatSubmit').addEventListener('click', function (event) {
     event.preventDefault();
-    const message = document.getElementById('botInput').value;
+    var chatbox = document.getElementById('chatbox').innerHTML;
+    const userMessage = document.getElementById('chatInput').value;
+    chatbox = chatbox + '<p class="userMessage">' + userMessage + '</p>';
+    document.getElementById('chatbox').innerHTML = chatbox;
     const API_KEY = 'JWm9LD7K8RA3';
-    // const API_KEY = process.env.API_KEY
-    // get one api key at pgamerx.com/api/register
-    fetch(`https://api.pgamerx.com/v5/ai?message=${message}&server=main`, {
+    fetch(`https://api.pgamerx.com/v5/ai?message=${userMessage}&server=main`, {
         method: "get",
         headers: {
             "Authorization": API_KEY
@@ -16,10 +13,12 @@ document.getElementById('botSubmit').addEventListener('click', function (event) 
     }).then(function (response) {
         return response.json();
     }).then(function (json) {
-        document.getElementById('botOutput').innerHTML = json[0].response;
+        botMessage = '<p class="botMessage">' + json[0].response + '</p>';
+        document.getElementById('chatbox').innerHTML = chatbox + botMessage;
     });
 });
 
-document.getElementById('botInput').addEventListener('keypress', function () {
-    document.getElementsByClassName('pageObject').style = "background-color = red";
-});
+// document.getElementById('botInput').addEventListener('keypress', function () {
+//     var randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     document.getElementById('botInput').style.color = '#' + randomColor;
+// });
